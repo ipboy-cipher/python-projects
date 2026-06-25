5de2cc33218de40b973109a14886e77197d87599
António Neto <antonionetoipboy07@gmail.com>
2026-06-25 15:14:26 +0100

    Adiciona  calculadora base em Python

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+print("=== Calculadora Simples ===")
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+print("\nResultados:")
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
